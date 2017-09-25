ca9e1f69c9966ad517c6667a754afb3eab73f4c7
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-25 14:05:17 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,13 +7,13 @@
 //
  for($i = 1; $i < 2; $i++)
 {
-	$link = 'http://putusan.mahkamahagung.go.id/direktori/index-'.$i.'.html';
+	$link = 'https://putusan.mahkamahagung.go.id/direktori/index-'.$i.'.html';
 	$linkpages = file_get_html($link);
 	if($linkpages)
 	{
 		foreach($linkpages->find("//table[@class='tabledata']/tbody/tr/a") as $element)
 		{		
-			if(strpos($element->href, "//putusan.mahkamahagung.go.id/putusan"))
+			if(strpos($element->href, "https://putusan.mahkamahagung.go.id/putusan"))
 			{
 				$linkpages = file_get_html($element->href);
 				foreach($linkpages->find("/html/body/div/div/table/tbody/tr[2]/td/div[1]/div[2]") as $details)
