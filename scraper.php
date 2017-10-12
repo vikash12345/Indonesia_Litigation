a93d061e968ccbf30bd6701caa233826f4d2e4e6
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-12 11:56:05 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,38 +1,28 @@
-<?
+<?php
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
- require 'scraperwiki.php';
- require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
  for($i = 1; $i < 2; $i++)
 {
 	$link = 'http://putusan.mahkamahagung.go.id/direktori/index-'.$i.'.html';
-	$linkpages = file_get_html($link);
-	sleep(1000);
-	if($linkpages)
+	$pageload 	=	file_get_html($link);
+	if($pageload)
 	{
-		foreach($linkpages->find("//table[@class='tabledata']/tbody/tr/a") as $element)
-		{		
-			if(strpos($element->href, "http://putusan.mahkamahagung.go.id/putusan"))
+		foreach($pageload->find("//table[@class='tabledata']/tbody/tr/a") as $element)
+		{								 
+			if(strstr($element->href, "https://putusan.mahkamahagung.go.id/putusan"))
 			{
-				$linkpages = file_get_html($element->href);
-				foreach($linkpages->find("/html/body/div/div/table/tbody/tr[2]/td/div[1]/div[2]") as $details)
-				
+				$innerpage	=	file_get_html($element->href);
 				{
-							
-							$html_encoded 	= 	html_entity_decode($details);
-							$nameofcase 	=	$details->find("b",0)->plaintext;
-							$prlink 			=	$element->href;
-							
-							
-							  $record = array( 'profilepage' =>$prlink, 'linkofpage' => $link ,'nameofcase' => $nameofcase,'code' => $html_encoded);
-           scraperwiki::save(array('profilepage','linkofpage','nameofcase','code'), $record); 
-			   }
+					$Match		=	$innerpage->find("//td[plaintext^=Jenis Perkara]", 0);
+					echo $element->href.' This is link =>  ' .$Match->next_sibling().'<br/>';
 				}
-				
-		}	}
+			}
+		}
 	}
+	
+}
 	  
-
 ?>
