d2be17b1b36dce0efee2e8673ab7868a335479b8
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-17 16:03:36 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,30 +10,89 @@
 //46875
   for($i = 1; $i < 3; $i++)
 {
-	  $pageload = 'http://putusan.mahkamahagung.go.id/direktori/index-'.$i.'.html';
-	//$pageload = file_get_contents($url);
+	//  $pageload 
+	$cHeadres = array(
+      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language: en-US,en;q=0.5',
+      'Connection: Keep-Alive',
+      'Pragma: no-cache',
+      'Cache-Control: no-cache'
+     );
+
+ $MyWebsite =  'http://putusan.mahkamahagung.go.id/direktori/index-'.$i.'.html';
+
+ function dlPage($href) {
+  global $cHeadres;
+
   $ch = curl_init();
+  if($ch){
+   curl_setopt($ch, CURLOPT_URL, $href);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+   curl_setopt($ch, CURLOPT_HEADER, false);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+   $str = curl_exec($ch);
+   curl_close($ch);
 
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, $pageload);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+   $dom = new simple_html_dom();
+   $dom->load($str);
+   return $dom;
+  }
+ }
 
-// grab URL and pass it to the browser
-curl_exec($ch);
-
-// close cURL resource, and free up system resources
-curl_close($ch);
+$pageload = dlPage($MyWebsite);
 	  
 	  
-
-if($pageload)
+ if($pageload)
 	{
 		foreach($pageload->find("//table[@class='tabledata']/tbody/tr/a") as $element)
 		{	
 			
 			if(strstr($element->href, "https://putusan.mahkamahagung.go.id/putusan"))
 			{
-				$innerpage	=	file_get_contents($element->href);
+	$cHeadres = array(
+      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language: en-US,en;q=0.5',
+      'Connection: Keep-Alive',
+      'Pragma: no-cache',
+      'Cache-Control: no-cache'
+     );
+
+ //$MyWebsite = $element->href;
+
+ function linkpage($href) {
+  global $cHeadres;
+
+  $ch = curl_init();
+  if($ch){
+   curl_setopt($ch, CURLOPT_URL, $href);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+   curl_setopt($ch, CURLOPT_HEADER, false);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+   $str = curl_exec($ch);
+   curl_close($ch);
+
+   $dom = new simple_html_dom();
+   $dom->load($str);
+   return $dom;
+  }
+ }
+
+$innerpage = linkpage($element->href);
+				
+				
+				//$innerpage	=	file_get_contents($element->href);
 				{	
 					if($innerpage)
 					{
