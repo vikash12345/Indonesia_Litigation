4dcc5fb79cd818ee5f47dbf1991c3ab6250ad794
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-30 13:31:32 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@
 // including some code snippets below that you should find helpful
  //require 'scraperwiki.php';
  require 'scraperwiki.php';
- require 'simple_html_dom.php';
+ require 'scraperwiki/simple_html_dom.php';
  $cHeadres = array(
       'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
       'Accept-Language: en-US,en;q=0.5',
@@ -34,10 +34,11 @@
          return $dom;
         }
        }
-//46875
-  for($i = 1; $i < 3; $i++)
+       
+for($page = 1; $page <3; $page++)
 {
-	 $link = 'https://e.fbr.gov.pk/Registration/searchDetail.aspx?crup='.$i;
+    
+ $link = 'https://e.fbr.gov.pk/Registration/searchDetail.aspx?crup='.$page;
 	$pageload = dlPage($link);
 	  
 	  
