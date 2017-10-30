abc4b300b9844fd4d0be58747af09edc86e968d7
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-30 17:19:36 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,14 +4,41 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
        
+$cHeadres = array(
+      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language: en-US,en;q=0.5',
+      'Connection: Keep-Alive',
+      'Pragma: no-cache',
+      'Cache-Control: no-cache'
+     );
+
+
+     function dlPage($link) {
+        global $cHeadres;
+        $ch = curl_init();
+        if($ch){
+         curl_setopt($ch, CURLOPT_URL, $link);
+         curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
+         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+         curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+         curl_setopt($ch, CURLOPT_HEADER, false);
+         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+         $str = curl_exec($ch);
+         curl_close($ch);
+         $dom = new simple_html_dom();
+         $dom->load($str);
+         return $dom;
+        }
+       }
+       
 for($page = 1; $page <2; $page++)
 {
     	$linkcreate	=	'http://putusan.mahkamahagung.go.id/direktori/index-'.$page.'.html';
-	$pageload = file_get_html($linkcreate);
-	if($pageload)
-	{
-	echo $pageload;
-	}
+	echo $linkcreate;
 }
    
 	
