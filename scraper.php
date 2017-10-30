d8c92731778fd84805bc8008fb732ffdfd61930e
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-30 17:13:30 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,43 +1,13 @@
 <?php
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
- //require 'scraperwiki.php';
 require 'scraperwiki.php';
-//require 'scraperwiki/simple_html_dom.php';
-require 'simple_html_dom.php';
- $cHeadres = array(
-      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Language: en-US,en;q=0.5',
-      'Connection: Keep-Alive',
-      'Pragma: no-cache',
-      'Cache-Control: no-cache'
-     );
-     function dlPage($link) {
-        global $cHeadres;
-        $ch = curl_init();
-        if($ch){
-         curl_setopt($ch, CURLOPT_URL, $link);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
-         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
-         curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
-         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-         curl_setopt($ch, CURLOPT_HEADER, false);
-         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
-         $str = curl_exec($ch);
-         curl_close($ch);
-         $dom = new simple_html_dom();
-         $dom->load($str);
-         return $dom;
-        }
-       }
+require 'scraperwiki/simple_html_dom.php';
        
 for($page = 1; $page <2; $page++)
 {
     	$linkcreate	=	'http://putusan.mahkamahagung.go.id/direktori/index-'.$page.'.html';
-	$pageload = dlPage($linkcreate);
+	$pageload = file_get_html($linkcreate);
 	if($pageload)
 	{
 	echo $pageload;
