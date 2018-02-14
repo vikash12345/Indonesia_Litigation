0e5c8775b09d3a24da51404cd0814d0c4666fe2c
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-14 09:16:35 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -35,7 +35,7 @@ $cHeadres = array(
         }
        }
        
-for($page = 1066; $page <49434; $page++)
+for($page = 1207; $page <49434; $page++)
 {
     	$link	=	'http://putusan.mahkamahagung.go.id/direktori/index-'.$page.'.html';
 	echo "$link\n";
