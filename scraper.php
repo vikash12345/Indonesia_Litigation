391e5a90c2482b00c01d475edcac22fa50e03016
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-05-12 10:09:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -35,7 +35,7 @@ $cHeadres = array(
         }
        }
        
-for($page = 7966; $page <49434; $page++)
+for($page = 8122; $page <49434; $page++)
 {
     	$link	=	'http://putusan.mahkamahagung.go.id/direktori/index-'.$page.'.html';
 	echo "$link\n";
